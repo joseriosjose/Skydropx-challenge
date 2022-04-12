a8374ec3b77d01e99cb6108a898e2af3ee8041a3
joseriosjose <jose_rios456@hotmail.com>
2022-04-11 19:24:11 -0500

    Component header

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+
+const NavBarWrapper = styled("div")(({ theme }) => ({
+  width: "100%",
+  position: "fixed",
+  webkitBoxShadow: "0px 0px 30px -18px #4E34E1",
+  boxShadow: "0px 0px 30px -18px #4E34E1",
+  background: "linear-gradient(160deg, #ffffff,#c6bdf9)",
+}));
+
+const LogoWrapper = styled("img")(({ theme }) => ({
+  margin: "20px 30px 20px 30px",
+  height: 50,
+  [theme.breakpoints.down("md")]: {
+    height: 30,
+  },
+}));
+
+const LOGO_SKYDROPX =
+  "https://skydropx.com/assets/landing5/skydropx_logo-4fb27c0601c3bccddd15cad09e612eb0d777dcdbcebae56c0382a8fe2978dfa9.svg";
 
 const NavBar = ({ ...props }) => {
-  return <div {...props}>NavBar</div>;
+  return (
+    <NavBarWrapper {...props}>
+      <LogoWrapper src={LOGO_SKYDROPX} />
+    </NavBarWrapper>
+  );
 };
 
 export default NavBar;
