31a3c23e1640a550c5919b0dce829830e49d3f2a
joseriosjose <jose_rios456@hotmail.com>
2022-04-14 12:44:15 -0500

    Env configured fix next vercel

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -1,11 +1,9 @@
 import http from "axios";
 
-const { NEXT_PUBLIC_BASE_API, NEXT_PUBLIC_TOKEN } = process.env
-
 const axiosConfig = http.create({
-    baseURL: `${NEXT_PUBLIC_BASE_API}`,
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_API}`,
     headers: {
-        Authorization: `Token token=${NEXT_PUBLIC_TOKEN}`,
+        Authorization: `Token token=${process.env.NEXT_PUBLIC_TOKEN}`,
         "Content-Type": "application/json",
     }
 });
